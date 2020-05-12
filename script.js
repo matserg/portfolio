f6f622c1d8bdf715a9bddff86dc5be0b3de42b47
matserg <61161987+matserg@users.noreply.github.com>
2020-05-13 00:11:31 +0300

    add boolean types

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var form = document.getElementById('form');
 function validate(event) {
   event.preventDefault();
-  var check = 0;
+  var check = false;
   check += inputValidation('name');
   check += inputValidation('email');
   check += inputValidation('subject');
   check += inputValidation('message');
-  if (check == 0) {
+  if (check == false) {
     var inner = document.getElementById('inner');
     inner.innerHTML = ('Спасибо, ваша заявка отправлена!');
   }
@@ -16,34 +16,30 @@ function inputValidation(id) {
   var inputNameValue = inputName && inputName.value;
   if (inputNameValue === '') {
     inputName.style.borderBottom = '5px solid red';
-    return 1;
+    return true;
   } else {
     inputName.style.borderBottom = '5px solid rgba(0, 0, 0, 0.5)';
-    return 0;
+    return false;
   }
 }
 form.addEventListener('submit', validate);
 
-
-
-
-
 // var form = document.getElementById('form');
 // function validate(event) {
 // 	event.preventDefault();
-// 	var check = 0;
+// 	var check = true;
 // 	var inputId = ['name', 'email', 'subject', 'message'];
 // 	for (var i = 0; i < inputId.length; i++) {
 // 		var inputName = document.getElementById(inputId[i]);
 // 		var inputNameValue = inputName && inputName.value;
 // 		if (inputNameValue === '') {
 // 			inputName.style.borderBottom = '5px solid red';
-// 			check += 1;
+// 			check = false;
 // 		} else {
 // 			inputName.style.borderBottom = '5px solid rgba(0, 0, 0, 0.5)';
 // 		}
 // 	}
-// 	if (check == 0) {
+// 	if (check === true) {
 // 		var inner = document.getElementById('inner2');
 // 		inner.style.opacity = 1;
 // 	}
