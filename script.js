f6840180b1d05bc736b1c7226e0283c53982d409
matserg <61161987+matserg@users.noreply.github.com>
2020-05-14 01:17:50 +0300

    last correction

--- script.js
+++ script.js
@@ -1,50 +1,51 @@
-var form = document.getElementById('form');
-function validate(event) {
-  event.preventDefault();
-  var check = false;
-  check += inputValidation('name');
-  check += inputValidation('email');
-  check += inputValidation('subject');
-  check += inputValidation('message');
-  if (check == false) {
-    var inner = document.getElementById('inner');
-    inner.innerHTML = ('Спасибо, ваша заявка отправлена!');
-  }
-}
-function inputValidation(id) {
-  var inputName = document.getElementById(id);
-  var inputNameValue = inputName && inputName.value;
-  if (inputNameValue === '') {
-    inputName.style.borderBottom = '5px solid red';
-    return true;
-  } else {
-    inputName.style.borderBottom = '5px solid rgba(0, 0, 0, 0.5)';
-    return false;
-  }
-}
-form.addEventListener('submit', validate);
-
 // var form = document.getElementById('form');
 // function validate(event) {
-// 	event.preventDefault();
-// 	var check = true;
-// 	var inputId = ['name', 'email', 'subject', 'message'];
-// 	for (var i = 0; i < inputId.length; i++) {
-// 		var inputName = document.getElementById(inputId[i]);
-// 		var inputNameValue = inputName && inputName.value;
-// 		if (inputNameValue === '') {
-// 			inputName.style.borderBottom = '5px solid red';
-// 			check = false;
-// 		} else {
-// 			inputName.style.borderBottom = '5px solid rgba(0, 0, 0, 0.5)';
-// 		}
-// 	}
-// 	if (check === true) {
-// 		var inner = document.getElementById('inner2');
-// 		inner.style.opacity = 1;
-// 	}
+//   event.preventDefault();
+//   isValid('name');
+//   isValid('email');
+//   isValid('subject');
+//   isValid('message');
+
+//   var isCorrect = isValid('name') && isValid('email') && isValid('subject') && isValid('message');
+//   if (isCorrect) {
+//     var inner = document.getElementById('inner');
+//     inner.innerHTML = ('Спасибо, ваша заявка отправлена!');
+//   }
+// }
+// function isValid(id) {
+//   var inputName = document.getElementById(id);
+//   var inputNameValue = inputName && inputName.value;
+//   if (inputNameValue === '') {
+//     inputName.style.borderBottom = '5px solid red';
+//     return false;
+//   } else {
+//     inputName.style.borderBottom = '5px solid rgba(0, 0, 0, 0.5)';
+//     return true;
+//   }
 // }
 // form.addEventListener('submit', validate);
+
+var form = document.getElementById('form');
+function validate(event) {
+	event.preventDefault();
+	var check = true;
+	var inputId = ['name', 'email', 'subject', 'message'];
+	for (var i = 0; i < inputId.length; i++) {
+		var inputName = document.getElementById(inputId[i]);
+		var inputNameValue = inputName && inputName.value;
+		if (inputNameValue === '') {
+			inputName.style.borderBottom = '5px solid red';
+			check = false;
+		} else {
+			inputName.style.borderBottom = '5px solid rgba(0, 0, 0, 0.5)';
+		}
+	}
+	if (check === true) {
+		var inner = document.getElementById('inner2');
+		inner.style.opacity = 1;
+	}
+}
+form.addEventListener('submit', validate);
 
 
 
